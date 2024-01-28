f871b2b6a7c52deaf7395d40c732948e2f18e992
Aitor Calderon <aitorclzamakola@gmail.com>
2024-01-28 20:16:59 +0100

    feat(legal-advise): legal-advise component

---
+++ src/app/legal/legal-advise/legal-advise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-advise',
+  standalone: true,
+  imports: [],
+  templateUrl: './legal-advise.component.html',
+  styleUrl: './legal-advise.component.scss'
+})
+export class LegalAdviseComponent {
+
+}
