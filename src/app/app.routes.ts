7091c6a0d8aef730e248e68a8cac71c11d302dcf
Aitor Calderon <aitorclzamakola@gmail.com>
2024-01-28 20:43:15 +0100

    feat(routes): Routes for legal pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {LegalAdviseComponent} from "./legal/legal-advise/legal-advise.component";
+import {PrivacyPolicyComponent} from "./legal/privacy-policy/privacy-policy.component";
+import {CookiesComponent} from "./legal/cookies/cookies.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'legal-advise', component: LegalAdviseComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'cookies', component: CookiesComponent },
 ];
