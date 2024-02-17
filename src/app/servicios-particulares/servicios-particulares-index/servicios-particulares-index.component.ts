d5f2a0e2457ac826e2b543ce77048d12418647f9
Aitor Calderon <aitorclzamakola@gmail.com>
2024-02-17 10:40:46 +0100

    feat(servicios-particulares): Add global styles and create view

--- src/app/servicios-particulares/servicios-particulares-index/servicios-particulares-index.component.ts
+++ src/app/servicios-particulares/servicios-particulares-index/servicios-particulares-index.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-servicios-particulares-index',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './servicios-particulares-index.component.html',
   styleUrl: './servicios-particulares-index.component.scss'
 })
 export class ServiciosParticularesIndexComponent {
-
+  routes = [
+    {path: 'asesoria-belleza', name: 'Asesoría de belleza'},
+    {path: 'analisis-morfologico', name: 'Análisis morfológico'},
+    {path: 'analisis-armario', name: 'Análisis de armario'},
+    {path: 'asesoria-bodas', name: 'Asesoría para bodas'},
+    {path: 'asesoria-hombres', name: 'Asesoría para hombres'},
+    {path: 'cambio-step', name: 'Cambio step by step'},
+    {path: 'healthy-styling', name: 'Healthy styling'},
+    {path: 'asesoria-habilidades-comunicativas', name: 'Habilidades comunicativas'}
+  ];
 }
