dd658b50d6c6e8b4f45fc1afcca5458ed2c7d18c
Aitor Calderon <aitorclzamakola@gmail.com>
2024-02-29 20:11:11 +0100

    feat(header): Remove expandible ul

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,8 +14,6 @@ import {filter} from "rxjs";
 export class HeaderComponent {
 
   isHamburgerActive: boolean | null = null;
-  showWeddingNav = false;
-
 
   constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document, private router: Router) {
     this.router.events.pipe(
@@ -23,10 +21,6 @@ export class HeaderComponent {
     ).subscribe(() => {
       this.renderer.removeClass(this.document.body, 'no-scroll');
     });
-  }
-
-  toggleWeddingNav() {
-    this.showWeddingNav = !this.showWeddingNav;
   }
 
   @HostListener('window:resize', ['$event'])
